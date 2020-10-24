c3d3533bf2425ac15cb39f0c1254cc4b31a8b062
Sergio Fenoll <sergio@fenoll.be>
2020-10-25 01:33:27 +0200

    Replace HelloWorld with equally useless welcome message

---
+++ src/components/Main.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-container>
+    <v-row class="text-center">
+      <v-col>
+        <h1>Hello there</h1>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "Main",
+};
+</script>
+
+<style scoped>
+</style>
